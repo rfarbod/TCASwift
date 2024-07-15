c1f3f9cfac26a76a3737c37e326e90116f1f303c
rfarbod <rfarbod@icloud.com>
2024-07-15 15:52:38 +0330

    Added the main files of the project

---
+++ TCASwift/TCASwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCASwift
+//
+//  Created by Farbod Rahiminik on 7/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCASwift/TCASwift/TCASwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASwiftApp.swift
+//  TCASwift
+//
+//  Created by Farbod Rahiminik on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TCASwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
