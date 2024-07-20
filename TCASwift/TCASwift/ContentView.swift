63b59f6d400f5c92a91786bc0cdc1e626a6600e6
rfarbod <rfarbod@icloud.com>
2024-07-20 05:59:48 +0330

    Added sign in and sign up buttons

--- TCASwift/TCASwift/ContentView.swift
+++ TCASwift/TCASwift/ContentView.swift
@@ -13,18 +13,62 @@ import SwiftUI
 struct ContentView: View {
     let store: StoreOf<JobReducer>
 
-    var body: some View {
-        WithPerceptionTracking {
-            ZStack {
-                VStack {
-                    JobAdsView(store: store)
-                }
-                .onAppear {
-                    store.send(.getJobs)
-                }
+    @State private var isShowingSignIn = false
+    @State private var isShowinSignUp = false
 
-                HStack {
-                    Button(
+    var body: some View {
+        NavigationView {
+            WithPerceptionTracking {
+                ZStack(alignment: .bottom) {
+                    VStack {
+                        JobAdsView(store: store)
+                    }
+                    .onAppear {
+                        store.send(.getJobs)
+                    }
+
+                    HStack {
+                        NavigationLink(destination: SignInView(), isActive: $isShowingSignIn) {
+                            Button(
+                                action: {
+                                    isShowingSignIn = true
+                                },
+                                label: {
+                                    Text("Sign In")
+                                        .font(.system(size: 15, weight: .bold))
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 15)
+                                                .stroke(Color.black, lineWidth: 1)
+                                        )
+                                }
+                            )
+                            .background(Color.green)
+                            .cornerRadius(15)
+                            .padding(10)
+                        }
+
+                        NavigationLink(destination: SignUpView(), isActive: $isShowinSignUp) {
+                            Button(
+                                action: {
+                                    isShowinSignUp = true
+                                },
+                                label: {
+                                    Text("Sign Up")
+                                        .font(.system(size: 15, weight: .bold))
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 15)
+                                                .stroke(Color.black, lineWidth: 1)
+                                        )
+                                }
+                            )
+                            .background(Color.white)
+                            .padding(10)
+                        }
+                    }
+                    .background(Color.white)
+                    .frame(height: 75)
                 }
             }
         }
